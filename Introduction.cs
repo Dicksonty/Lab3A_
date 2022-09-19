a01128bed8fcc13f18dc75fd798261c37956537b
Dicksonty <113867382+Dicksonty@users.noreply.github.com>
2022-09-19 00:04:11 -0400

    Add files via upload

---
+++ Introduction.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("What is your first name?");
+Console.WriteLine("Tyrese");
+Console.WriteLine();
+Console.WriteLine("What is your last name?");
+Console.WriteLine("Dickson");
+string firstname = "Tyrese";
+string lastname = " Dickson";
+Console.WriteLine();
+Console.WriteLine("How old are you");
+Console.WriteLine("21");
+Console.WriteLine();
+int age = 21;
+string firstNameInitial = "Tyrese";
+//                         012345
+string lastNameInitial = "Dickson";
+//                        0123456
+Console.WriteLine("Hello, " +  firstname + lastname + "! Your initials are " + firstNameInitial[0] + "." + lastNameInitial[0] + "." + ", and you are " + age + " years old.");
